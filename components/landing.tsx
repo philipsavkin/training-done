f55fc600ebaebaf4f7e24d6ba96acbfc38f1034b
philipsavkin <philip.savkin@gmail.com>
2023-05-05 12:00:16 +0300

    Request correct scope from Strava

--- components/landing.tsx
+++ components/landing.tsx
@@ -6,7 +6,7 @@ export function Landing() {
     <main className="min-h-screen p-24 pt-4">
       <h2>Landing</h2>
       <button
-        onClick={() => signIn('strava')}
+        onClick={() => signIn('strava', undefined, { scope: 'activity:read_all' })}
         type="button"
         className="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
